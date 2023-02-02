0319927162b88f36c3b342267cce4747c08b8e51
NikitaGutenev <gutenev229@gmail.com>
2023-02-02 20:36:15 +0300

    Создал вывод 1000 частиц в виде куба

--- project.py
+++ project.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 #класс частицы
 class system():
     def __init__(self, x, y, z):
         self.coord = (x, y, z)
 
+
+# создание куба с частицами
 cube = []
 for x in range(10):
     for y in range(10):
@@ -18,17 +22,28 @@ vertices = np.array(
     ]
 )
 
-plt.plot_verticles(vertices = vertices, isosurf = False)
-exit()
-#Это окно
-tk = Tk()
-tk.protocol("WM_DELETE_WINDOW")
-tk.title("куб")
-tk.resizable(0,0)
-tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=900, height=600)
-canvas.pack()
-tk.mainloop()
+#функция для вывода частиц
+def plot_verticles(vertices, isosurf = False, filename = None):
+    # Create a new plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x = [v[0] for v in vertices]
+    y = [v[1] for v in vertices]
+    z = [v[2] for v in vertices]    
+    if isosurf:
+        ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True)
+    else:
+        ax.scatter(x, y, z, c='r', marker='o')    
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    # Show or save the plot
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(filename)
+
+plot_verticles(vertices = vertices, isosurf = False)
 
 
 
