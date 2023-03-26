a1af2724c8a79fd6624190e60f4de5b297ccc19e
Nikita_Gutenev <gutenev229@gmail.com>
2023-03-26 20:44:00 +0300

    немного фикс

--- project.py
+++ project.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
 import math
-import matplotlib.animation as animation
-import numpy as np
 from celluloid import Camera
 
 #класс частицы
@@ -10,9 +8,15 @@ class system():
         r = math.sqrt(x**2 + y**2 + z**2)
         m = 10
         # силы тяжести частицы
-        self.Fx = x/math.fabs(r)**13 - x/math.fabs(r)**7
-        self.Fy = y/math.fabs(r)**13 - y/math.fabs(r)**7
-        self.Fz = z/math.fabs(r)**13 - z/math.fabs(r)**7
+        # self.Fx = x/math.fabs(r)**13 - x/math.fabs(r)**7
+        # self.Fy = y/math.fabs(r)**13 - y/math.fabs(r)**7
+        # self.Fz = z/math.fabs(r)**13 - z/math.fabs(r)**7
+        # self.Fx = (16-x)/math.fabs(r)**13 - (16-x)/math.fabs(r)**7
+        # self.Fy = (16-y)/math.fabs(r)**13 - (16-y)/math.fabs(r)**7
+        # self.Fz = (16-z)/math.fabs(r)**13 - (16-z)/math.fabs(r)**7
+        self.Fx = min(x,16-x)
+        self.Fy = min(y,16-y)
+        self.Fz = min(z,16-z)
         #её координаты
         self.coord = [x, y, z]
         #ускорения частиц
@@ -21,8 +25,8 @@ class system():
         self.Az= self.Fz/m
         #скорость частиц
         self.vx= 1
-        self.vy= 1
-        self.vz= 1
+        self.vy= 5
+        self.vz= 10
         
 
 # создание куба с частицами
@@ -44,38 +48,22 @@ def step(vertices,ax):
         ax.scatter(vertices[item].coord[0], vertices[item].coord[1], vertices[item].coord[2], c='r', marker='.')
 
 def lines(ax):
-    edge_vertices = [
-    [0,0,0],
-    [16,0,0],
-    [16,16,0],
-    [0,16,0],
-    [0,0,16],
-    [16,0,16],
-    [16,16,16],
-    [0,16,16]]
+    edge_vertices = [[0,0,0],[16,0,0],[16,16,0],
+                    [0,16,0],[0,0,16],[16,0,16],
+                    [16,16,16],[0,16,16]]
     x = [v[0] for v in edge_vertices]
     y = [v[1] for v in edge_vertices]
     z = [v[2] for v in edge_vertices]
     ax.scatter(x, y, z,marker ='o')
     # Connect the vertices to form the edges of the cube
-    edges = [    [0,1],
-        [1,2],
-        [2,3],
-        [3,0],
-        [4,5],
-        [5,6],
-        [6,7],
-        [7,4],
-        [0,4],
-        [1,5],
-        [2,6],
-        [3,7]
-    ]
+    edges = [    [0,1],[1,2],[2,3],[3,0],
+                [4,5],[5,6],[6,7],[7,4],
+                [0,4],[1,5],[2,6],[3,7]]
     for edge in edges:
         ax.plot3D(*zip(edge_vertices[edge[0]], edge_vertices[edge[1]]), color='blue')
 
 #функция для вывода частиц
-def plot_verticles(vertices, isosurf = False, filename = None, borders = None):
+def plot_verticles(vertices):
     # Create a new plot
     fig = plt.figure()
     camera = Camera(fig)
@@ -85,19 +73,17 @@ def plot_verticles(vertices, isosurf = False, filename = None, borders = None):
     ax.set_zlabel('Z')
 
 
-    # Show or save the plot
-    if filename is None:
-        for i in range(10):
-            step(vertices,ax)
-            lines(ax)
-            plt.show()
-            camera.snap()
-        animation = camera.animate()
-        animation.save('dots.gif', writer = 'imagemagick')
-    else:
-        plt.savefig(filename)    
+    # Show the plot
+    for i in range(10):
+        step(vertices,ax)
+        lines(ax)
+        plt.show()
+        camera.snap()
+    animation = camera.animate()
+    animation.save('dots.gif', writer = 'imagemagick')
+    
 
 #вызов функций
-plot_verticles(cube, isosurf = False, filename = None, borders = None)
+plot_verticles(cube)
 
 
