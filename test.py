7bd25a59c12e918369c0c57893f9f48fd265dbbc
Nikita_Gutenev <gutenev229@gmail.com>
2023-04-29 12:09:52 +0300

    добавил в test цикл и функцию для взаимодействия частиц. Строки 74 и 95

---
+++ test.py
@@ -0,0 +1,166 @@
+import matplotlib.pyplot as plt
+import math
+from celluloid import Camera
+import random
+#класс частицы
+class system():
+          
+    def __init__(self, x, y, z):
+        self.m = 10
+        # силы тяжести частицы
+        # self.Fx = x/math.fabs(r)**13 - x/math.fabs(r)**7
+        # self.Fy = y/math.fabs(r)**13 - y/math.fabs(r)**7
+        # self.Fz = z/math.fabs(r)**13 - z/math.fabs(r)**7
+        # self.Fx = (16-x)/math.fabs(r)**13 - (16-x)/math.fabs(r)**7
+        # self.Fy = (16-y)/math.fabs(r)**13 - (16-y)/math.fabs(r)**7
+        # self.Fz = (16-z)/math.fabs(r)**13 - (16-z)/math.fabs(r)**7
+        self.Fx = 1/self.module_comparisonx(x)
+        self.Fy = 1/self.module_comparisony(y)
+        self.Fz = 1/self.module_comparisonz(z)
+        #её координаты
+        self.coord = [x, y, z]
+        #ускорения частиц
+        self.Ax= self.Fx/self.m
+        self.Ay= self.Fy/self.m
+        self.Az= self.Fz/self.m
+        #скорость частиц
+        self.vx= random.random() - 0.5
+        self.vy= random.random() - 0.5
+        self.vz= random.random() - 0.5
+   
+
+# вычисляет расстояние до близжайшей границы и осуществляет неупругое отталкивание по оси X
+    def module_comparisonx(self,a):
+        if abs(a) < abs(a - 16):
+            if a<0:
+                self.vx = abs(self.vx) / 2
+            return abs(a)
+        else:
+            if a>16:
+                self.vx = - abs(self.vx) / 2
+            return -abs(a - 16)  
+        
+# вычисляет расстояние до близжайшей границы и осуществляет неупругое отталкивание по оси Y
+    def module_comparisony(self,a):
+        if abs(a) < abs(a - 16):
+            if a<0:
+                self.vy = abs(self.vy) / 2
+            return abs(a)
+        else:
+            if a>16:
+                self.vy = - abs(self.vy) / 2
+            return -abs(a - 16)
+        
+# вычисляет расстояние до близжайшей границы и осуществляет неупругое отталкивание по оси Z
+    def module_comparisonz(self,a):
+        if abs(a) < abs(a - 16):
+            if a<0:
+                self.vz = abs(self.vz) / 2
+            return abs(a)
+        else:
+            if a>16:
+                self.vz = - abs(self.vz) / 2
+            return -abs(a - 16)             
+    
+cube = []
+for x in range(6,11):
+    for y in range(6,11):
+        for z in range(6,11):
+            p = system(x,y,z)
+            cube.append(p)
+
+# 1 шаг частицы с учетом всех координат
+def step(vertices,ax):
+    def raast(a,b):
+        '''функция нахождения расстояния между частицами'''
+        return math.sqrt((a.coord[0]-b.coord[0])**2 + (a.coord[1]-b.coord[1])**2 + (a.coord[2]-b.coord[2])**2)
+
+    for item in range(125):
+        vertices[item].coord[0] += vertices[item].vx
+        vertices[item].coord[1] += vertices[item].vy
+        vertices[item].coord[2] += vertices[item].vz
+        vertices[item].vx += vertices[item].Ax
+        vertices[item].vy += vertices[item].Ay
+        vertices[item].vz += vertices[item].Az
+
+        #изменение силы взаимодействия со стенкой и вычисление ускорения
+        vertices[item].Fx = 1/(vertices[item].module_comparisonx(vertices[item].coord[0]))
+        vertices[item].Fy = 1/(vertices[item].module_comparisony(vertices[item].coord[1]))
+        vertices[item].Fz = 1/(vertices[item].module_comparisonz(vertices[item].coord[2]))
+        vertices[item].Ax= vertices[item].Fx**3/vertices[item].m
+        vertices[item].Ay= vertices[item].Fy**3/vertices[item].m
+        vertices[item].Az= vertices[item].Fz**3/vertices[item].m
+
+        #взаимодействие между часицами
+        chast = 124*[0]
+        mini = 1
+        for i in range(1,125):
+            if ((distantion:=raast(vertices[0],vertices[item]))<mini and item!=0):
+                chast[i] = distantion
+        
+        #ограничение ускорения (предельное ускорение)
+        if vertices[item].Ax > 0.2:
+            vertices[item].Ax = 0.2
+        if vertices[item].Ay > 0.2:
+            vertices[item].Ay = 0.2
+        if vertices[item].Az > 0.2:
+            vertices[item].Az = 0.2
+        if vertices[item].Ax < -0.2:
+            vertices[item].Ax = -0.2
+        if vertices[item].Ay < -0.2:
+            vertices[item].Ay = -0.2
+        if vertices[item].Az < -0.2:
+            vertices[item].Az = -0.2    
+
+        #обновление частиц
+        ax.scatter(vertices[item].coord[0], vertices[item].coord[1], vertices[item].coord[2], c='r', marker='.')
+
+
+#границы куба
+def lines(ax):
+    edge_vertices = [[0,0,0],[16,0,0],[16,16,0],
+                    [0,16,0],[0,0,16],[16,0,16],
+                    [16,16,16],[0,16,16]]
+    x = [v[0] for v in edge_vertices]
+    y = [v[1] for v in edge_vertices]
+    z = [v[2] for v in edge_vertices]
+    ax.scatter(x, y, z,marker ='o')
+    # Создание ребер куба путем соединения точек вершин
+    edges = [    [0,1],[1,2],[2,3],[3,0],
+                [4,5],[5,6],[6,7],[7,4],
+                [0,4],[1,5],[2,6],[3,7]]
+    for edge in edges:
+        ax.plot3D(*zip(edge_vertices[edge[0]], edge_vertices[edge[1]]), color='blue')
+
+#функция для вывода частиц
+def plot_verticles(vertices):
+    # Создание модели
+    fig = plt.figure()
+    camera = Camera(fig)
+    ax = fig.add_subplot(111, projection='3d') 
+    x = [v.coord[0] for v in vertices]
+    y = [v.coord[1] for v in vertices]
+    z = [v.coord[2] for v in vertices]    
+    ax.scatter(x, y, z, c='r', marker='.')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+
+    # Создание гиф
+    lines(ax)
+    camera.snap()
+    for i in range(70): # кол-во кадров
+        step(vertices,ax)
+        lines(ax)
+        camera.snap()
+    animation = camera.animate()
+    animation.save('dots.gif', writer = 'imagemagick')
+    
+
+#вызов функций
+plot_verticles(cube)
+print('ГОТОВО ЧЕКАЙ ГИФКУ')
+
+
+ 
